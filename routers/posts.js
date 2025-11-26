983456499054d5e5a3c79c9cd342161947662955
giannivitale <giavitale78@gmail.com>
2025-11-26 18:38:40 +0100

    Refactor articles controller: consolidate logic into a single file and update routing

--- routers/posts.js
+++ routers/posts.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const articlesController = require('../controllers/controller');
+const articlesController = require('../controllers/articlesController');
 
 
 
